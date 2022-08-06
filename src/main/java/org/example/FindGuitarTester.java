1b9cc2a97ec97bf855351a729e7020ca75353ad4
Jesus Bojorquez <jesusbojorquez@MacBook-Pro-di-Jesus.local>
2022-08-06 21:46:01 +0200

    added enum to improve robustness

--- src/main/java/org/example/FindGuitarTester.java
+++ src/main/java/org/example/FindGuitarTester.java
@@ -8,7 +8,10 @@ public class FindGuitarTester {
         Inventory inventory = new Inventory();
         initializeInventory(inventory);
 
-        Guitar whatErinLikes = new Guitar("",0,"fender","Stratocastor","electric","Alder","Alder");
+        Guitar whatErinLikes = new Guitar(
+                "",0,Builder.FENDER.toString(),"Stratocastor",
+                Type.ELECTRIC.toString(), Wood.ALDER.toString(), Wood.ALDER.toString()
+        );
 
         Guitar guitar = inventory.search(whatErinLikes);
         if(guitar != null) {
@@ -24,27 +27,38 @@ public class FindGuitarTester {
     }
 
     private static void initializeInventory(Inventory inventory) {
-        inventory.addGuitar("11277", 3999.95, "Collings", "CJ", "acoustic",
-                "Indian Rosewood", "Sitka");
-        inventory.addGuitar("V95693", 1499.95, "Fender", "Stratocastor", "electric",
-                "Alder", "Alder");
-        inventory.addGuitar("V9512", 1549.95, "Fender", "Stratocastor", "electric",
-                "Alder", "Alder");
-        inventory.addGuitar("122784", 5495.95, "Martin", "D-18", "acoustic",
-                "Mahogany", "Adirondack");
-        inventory.addGuitar("76531", 6295.95, "Martin", "OM-28", "acoustic",
-                "Brazilian Rosewood", "Adriondack");
-        inventory.addGuitar("70108276", 2295.95, "Gibson", "Les Paul", "electric",
-                "Mahogany", "Maple");
-        inventory.addGuitar("82765501", 1890.95, "Gibson", "SG '61 Reissue",
-                "electric", "Mahogany", "Mahogany");
-        inventory.addGuitar("77023", 6275.95, "Martin", "D-28", "acoustic",
-                "Brazilian Rosewood", "Adirondack");
-        inventory.addGuitar("1092", 12995.95, "Olson", "SJ", "acoustic",
-                "Indian Rosewood", "Cedar");
-        inventory.addGuitar("566-62", 8999.95, "Ryan", "Cathedral", "acoustic",
-                "Cocobolo", "Cedar");
-        inventory.addGuitar("6 29584", 2100.95, "PRS", "Dave Navarro Signature",
-                "electric", "Mahogany", "Maple");
+        inventory.addGuitar("11277", 3999.95, Builder.COLLINGS.toString(),
+                "CJ", Type.ACOUSTIC.toString(),
+                Wood.INDIAN_ROSEWOOD.toString(), Wood.SITKA.toString());
+        inventory.addGuitar("V95693", 1499.95, Builder.FENDER.toString(),
+                "Stratocastor", Type.ELECTRIC.toString(),
+                Wood.ALDER.toString(), Wood.ALDER.toString());
+        inventory.addGuitar("V9512", 1549.95, Builder.FENDER.toString(),
+                "Stratocastor", Type.ELECTRIC.toString(),
+                Wood.ALDER.toString(), Wood.ALDER.toString());
+        inventory.addGuitar("122784", 5495.95, Builder.MARTIN.toString(),
+                "D-18", Type.ACOUSTIC.toString(),
+                Wood.MAHOGANY.toString(), Wood.ADIRONDACK.toString());
+        inventory.addGuitar("76531", 6295.95, Builder.MARTIN.toString(),
+                "OM-28", Type.ACOUSTIC.toString(),
+                Wood.BRAZILIAN_ROSEWOOD.toString(), Wood.ADIRONDACK.toString());
+        inventory.addGuitar("70108276", 2295.95, Builder.GIBSON.toString(),
+                "Les Paul", Type.ELECTRIC.toString(),
+                Wood.MAHOGANY.toString(), Wood.MAHOGANY.toString());
+        inventory.addGuitar("82765501", 1890.95, Builder.GIBSON.toString(),
+                "SG '61 Reissue", Type.ELECTRIC.toString(),
+                Wood.MAHOGANY.toString(), Wood.MAHOGANY.toString());
+        inventory.addGuitar("77023", 6275.95, Builder.MARTIN.toString(),
+                "D-28", Type.ACOUSTIC.toString(),
+                Wood.BRAZILIAN_ROSEWOOD.toString(), Wood.ADIRONDACK.toString());
+        inventory.addGuitar("1092", 12995.95, Builder.OLSON.toString(),
+                "SJ", Type.ACOUSTIC.toString(),
+                Wood.INDIAN_ROSEWOOD.toString(), Wood.CEDAR.toString());
+        inventory.addGuitar("566-62", 8999.95, Builder.RYAN.toString(),
+                "Cathedral", Type.ACOUSTIC.toString(),
+                Wood.COCOBOLO.toString(), Wood.CEDAR.toString());
+        inventory.addGuitar("6 29584", 2100.95, Builder.PRS.toString(),
+                "Dave Navarro Signature", Type.ELECTRIC.toString(),
+                Wood.MAHOGANY.toString(), Wood.MAPLE.toString());
     }
 }
--- src/main/java/org/example/Inventory.java
+++ src/main/java/org/example/Inventory.java
@@ -31,21 +31,18 @@ public class Inventory {
             Guitar guitar = (Guitar) i.next();
             // Ignore serial number since that's unique
             // Ignore price since that's unique
-            String builder = searchGuitar.getBuilder();
-            if ((builder != null) && (!builder.equals("")) && (!builder.equals(guitar.getBuilder())))
+            if(searchGuitar.getBuilder() != guitar.getBuilder())
                 continue;
-            String model = searchGuitar.getModel();
-            if((model != null) && (!model.equals("")) && (!model.equals(guitar.getModel())))
+            String model = searchGuitar.getModel().toLowerCase();
+            if((model != null) && (!model.equals("")) && (!model.equals(guitar.getModel().toLowerCase())))
                 continue;
-            String type = searchGuitar.getType();
-            if((type != null) && (!type.equals("")) && (!type.equals(guitar.getType())))
+            if(searchGuitar.getType() != guitar.getType())
                 continue;
-            String backWood = searchGuitar.getBackWood();
-            if((backWood != null) && (!backWood.equals("")) && (!backWood.equals(guitar.getBackWood())))
+            if(searchGuitar.getBackWood() != guitar.getBackWood())
                 continue;
-            String topWood = searchGuitar.getTopWood();
-            if((topWood != null) && (!topWood.equals("")) && (!topWood.equals(guitar.getTopWood())))
+            if(searchGuitar.getTopWood() != guitar.getTopWood())
                 continue;
+            return guitar;
         }
         return null;
     }
